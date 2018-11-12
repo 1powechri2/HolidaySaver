0c29cbb59e7ec32ba3eee399243b0430145ed5a2
Chris Powell <chris.ian.powell@gmail.com>
2018-11-12 15:44:27 -0700

    Adds function to quit subscription

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'root#show'
   post '/sign_up', to: 'sign_up#create'
   post '/log_in', to: 'log_in#login'
+  post '/delete_user', to: 'log_in#delete'
 
   namespace :api do
     namespace :v1 do
